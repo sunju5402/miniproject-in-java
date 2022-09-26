81c5aac066cdb671a5d1ee9a927f0b0e40032394
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2022-09-26 23:44:43 +0900

    simplify the code

--- MiniProject03.java
+++ MiniProject03.java
@@ -1,6 +1,3 @@
-/*
-이선주
-*/
 import java.util.Scanner;
 
 public class MiniProject03 {
@@ -20,16 +17,14 @@ public class MiniProject03 {
         int fee = 10000;
         if (age < 3) {
             fee = 0;
-        } else if (age < 13) {
+        } else if (age < 13 || time >= 17) {
             fee = 4000;
         } else {
-          if (time >= 17) {
-              fee = 4000;
-          } else {
-              if (c1 == 'y' || c2 == 'y') {
+            if (time < 17) {
+                if (c1 == 'y' || c2 == 'y') {
                   fee = 8000;
-              }
-          }
+                }
+            }
         }
         System.out.println("입장료: " + fee);
     }

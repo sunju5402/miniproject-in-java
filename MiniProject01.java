f63dd70be607fe155900cc069ff14b973c082bb4
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2022-09-27 12:21:14 +0900

    Update MiniProject01.java

--- MiniProject01.java
+++ MiniProject01.java
@@ -1,11 +1,11 @@
 /*
-이선주
+구구단
 */
 public class MiniProject01 {
     public static void main(String[] args) {
         System.out.println("[구구단 출력]");
         for (int i = 1; i < 10; i++) {
-            for (int j = 1; j < 10; j++) {
+            for (int j = 1; j <= 9; j++) {
                 System.out.printf("%02d x %02d = %02d\t", j, i, j * i);
             }
             System.out.println();
